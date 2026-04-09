7f40ed467cc015f53dfe85cca5f72ca7b3fcb41c
JustinTBurns <justinburns2008@gmail.com>
2026-04-09 14:59:00 -0700

    Fixed a non File-Scoped namespace.

--- DoodleNote/Data/Migrations/20260409215151_DoodleNoteProperties.cs
+++ DoodleNote/Data/Migrations/20260409215151_DoodleNoteProperties.cs
@@ -3,34 +3,33 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace DoodleNote.Data.Migrations
+namespace DoodleNote.Data.Migrations;
+
+/// <inheritdoc />
+public partial class DoodleNoteProperties : Migration
 {
     /// <inheritdoc />
-    public partial class DoodleNoteProperties : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "DoodleNotes",
-                columns: table => new
-                {
-                    NoteId = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    NoteTitle = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
-                    CreatedDate = table.Column<DateTime>(type: "date", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_DoodleNotes", x => x.NoteId);
-                });
-        }
+        migrationBuilder.CreateTable(
+            name: "DoodleNotes",
+            columns: table => new
+            {
+                NoteId = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                NoteTitle = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                CreatedDate = table.Column<DateTime>(type: "date", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_DoodleNotes", x => x.NoteId);
+            });
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "DoodleNotes");
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "DoodleNotes");
     }
 }
