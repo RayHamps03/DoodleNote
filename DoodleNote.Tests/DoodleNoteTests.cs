116c98c3b9341f022a99232077030726fecb7f62
JB-Coding02 <justinburns2008@gmail.com>
2026-04-30 10:35:23 -0700

    Updated namespaces to use file-scoped
    Noticed that tests in the DoodleNote.Tests project broke will fix in the next commit... hopefully.

--- DoodleNote.Tests/DoodleNoteTests.cs
+++ DoodleNote.Tests/DoodleNoteTests.cs
@@ -2,29 +2,28 @@ using DoodleNote.Models;
 using Xunit;
 using System;
 
-namespace DoodleNote.Tests
-{
-    public class DoodleNoteTests
-    {
-        [Fact]
-        public void CanSetAndGetProperties()
-        {
-            var note = new DoodleNote.Models.DoodleNote
-            {
-                NoteId = 1,
-                NoteTitle = "Test Note",
-                CreatedDate = new DateTime(2024, 4, 10)
-            };
-            Assert.Equal(1, note.NoteId);
-            Assert.Equal("Test Note", note.NoteTitle);
-            Assert.Equal(new DateTime(2024, 4, 10), note.CreatedDate);
-        }
+namespace DoodleNote.Tests;
 
-        [Fact]
-        public void CreatedDate_StoresDateOnly()
+public class DoodleNoteTests
+{
+    [Fact]
+    public void CanSetAndGetProperties()
+    {
+        var note = new DoodleNote.Models.DoodleNote
         {
-            var note = new DoodleNote.Models.DoodleNote { NoteTitle = "Test", CreatedDate = new DateTime(2024, 4, 10, 15, 30, 0) };
-            Assert.Equal(new DateTime(2024, 4, 10), note.CreatedDate);
-        }
+            NoteId = 1,
+            NoteTitle = "Test Note",
+            CreatedDate = new DateTime(2024, 4, 10)
+        };
+        Assert.Equal(1, note.NoteId);
+        Assert.Equal("Test Note", note.NoteTitle);
+        Assert.Equal(new DateTime(2024, 4, 10), note.CreatedDate);
+    }
+
+    [Fact]
+    public void CreatedDate_StoresDateOnly()
+    {
+        var note = new DoodleNote.Models.DoodleNote { NoteTitle = "Test", CreatedDate = new DateTime(2024, 4, 10, 15, 30, 0) };
+        Assert.Equal(new DateTime(2024, 4, 10), note.CreatedDate);
     }
 }
