27ef4ee62ccab1a7f45f4ad00c7e222a1fad03dd
RayHamps03 <rayhampson4@gmail.com>
2026-05-25 14:58:55 -0700

    Added migration for DoodleNote image property for database compatability

---
+++ DoodleNote/Data/Migrations/20260525215805_DoodleNoteImageUrlProperty.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoodleNote.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DoodleNoteImageUrlProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "DoodleNotes",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "DoodleNotes");
+        }
+    }
+}
