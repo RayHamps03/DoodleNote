b2b20c5fdadd27f7fa6225ade717255bb06e9277
RayHamps03 <rayhampson4@gmail.com>
2026-07-19 15:48:12 -0700

    Added create function to UploadViewModel to allow creation when initiating a doodle note upload

--- DoodleNote/Features/DoodleUpload/Models/UploadViewModels.cs
+++ DoodleNote/Features/DoodleUpload/Models/UploadViewModels.cs
@@ -21,4 +21,9 @@ public sealed class ConfirmDoodleUploadViewModel
 
 	[StringLength(300)]
 	public string? Description { get; set; }
+
+	public static DoodleNote.Models.DoodleNote Create(string noteTitle, string? description, string? imagePath)
+	{
+		return DoodleNote.Models.DoodleNote.Create(noteTitle, description, null);
+	}
 }
