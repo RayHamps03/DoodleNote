9d8e50003672f2dab6f109c7d5d3a7c8e2f30f87
Raymond Hampson III <rayhampson4@gmail.com>
2026-08-10 16:53:06 -0700

    Added ApplicationUser and DoodleNote navigation properties to UserLike

--- DoodleNote/Models/UserLike.cs
+++ DoodleNote/Models/UserLike.cs
@@ -1,7 +1,13 @@
-﻿namespace DoodleNote.Models;
+﻿using DoodleNote.Features.Admin.Models;
+namespace DoodleNote.Models;
 
 public class UserLike
 {
+	
 	public string UserId { get; set; } = string.Empty;
 	public int NoteId { get; set; }
+
+	public ApplicationUser User { get; set; } = null!;
+
+	public DoodleNote Note { get; set; } = null!;
 }
