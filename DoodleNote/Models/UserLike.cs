a6defdd28db8984b24592e79905e831687e667fe
Raymond Hampson III <rayhampson4@gmail.com>
2026-08-10 22:01:19 -0700

    Add documentation to UserLike class properties

--- DoodleNote/Models/UserLike.cs
+++ DoodleNote/Models/UserLike.cs
@@ -3,11 +3,15 @@ namespace DoodleNote.Models;
 
 public class UserLike
 {
-	
+	// The UserId associated with the user who liked the note.
 	public string UserId { get; set; } = string.Empty;
+
+	// The NoteId associated with the note that was liked.
 	public int NoteId { get; set; }
 
+	// Navigation property to the ApplicationUser entity.
 	public ApplicationUser User { get; set; } = null!;
 
+	// Navigation property to the DoodleNote entity.
 	public DoodleNote Note { get; set; } = null!;
 }
