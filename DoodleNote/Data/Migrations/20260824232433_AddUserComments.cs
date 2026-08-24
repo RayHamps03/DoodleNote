4a14eeb04d230c06ea68e6ab0c62705803bedf29
RayHamps03 <rayhampson4@gmail.com>
2026-08-24 16:25:30 -0700

    Added migration for recent database changes

---
+++ DoodleNote/Data/Migrations/20260824232433_AddUserComments.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoodleNote.Data.Migrations;
+
+    /// <inheritdoc />
+    public partial class AddUserComments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserComments",
+                columns: table => new
+                {
+                    CommentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CommentText = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    NoteId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserComments", x => x.CommentId);
+                    table.ForeignKey(
+                        name: "FK_UserComments_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_UserComments_DoodleNotes_NoteId",
+                        column: x => x.NoteId,
+                        principalTable: "DoodleNotes",
+                        principalColumn: "NoteId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserComments_NoteId",
+                table: "UserComments",
+                column: "NoteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserComments_UserId",
+                table: "UserComments",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserComments");
+        }
+    }
