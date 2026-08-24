8119340faa6db730f63147018b056355c3e91a86
RayHamps03 <rayhampson4@gmail.com>
2026-08-24 16:24:18 -0700

    Set CommentId as primary key and added comment length restraints

--- DoodleNote/Models/UserComment.cs
+++ DoodleNote/Models/UserComment.cs
@@ -1,10 +1,15 @@
-﻿namespace DoodleNote.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DoodleNote.Models;
 
 public class UserComment
 {
+	[Key]
 	// Unique identifier for the comment
 	public int CommentId { get; set; }
 	// Text to be displayed as the comment
+	[Required]
+	[StringLength(300, ErrorMessage = "Comment cannot exceed 300 characters.")]
 	public string CommentText { get; set; } = string.Empty;
 	// Foreign key to ApplicationUser
 	public required string UserId { get; set; }
