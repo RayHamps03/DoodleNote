56a74b8e76d99a984d12a04086fa4ac382bc7350
RayHamps03 <rayhampson4@gmail.com>
2026-08-24 16:13:37 -0700

    Added comment properties for an associated user and post

--- DoodleNote/Models/UserComment.cs
+++ DoodleNote/Models/UserComment.cs
@@ -2,4 +2,19 @@
 
 public class UserComment
 {
+	// Primary key
+	public int CommentId { get; set; }
+	// Foreign key to ApplicationUser
+	public required string UserId { get; set; }
+	// Foreign key to DoodleNote
+	public required int NoteId { get; set; }
+
+
+	// ApplicationUser navigation property
+	public ApplicationUser? User { get; set; }
+	// DoodleNote navigation property
+	public DoodleNote Note { get; set; } 
+
+	public 
+
 }
